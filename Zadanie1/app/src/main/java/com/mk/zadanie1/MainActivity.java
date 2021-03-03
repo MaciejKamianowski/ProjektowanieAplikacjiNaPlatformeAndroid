627adea003267a4f99c72c9455d7fd65d708a934
Maciej Kamianowski <2605mac@gmail.com>
2021-03-03 10:23:41 +0100

    Kalkulator działa

--- Zadanie1/app/src/main/java/com/mk/zadanie1/MainActivity.java
+++ Zadanie1/app/src/main/java/com/mk/zadanie1/MainActivity.java
@@ -3,9 +3,12 @@ package com.mk.zadanie1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private EditText firstNumberInput;
@@ -22,8 +25,10 @@ public class MainActivity extends AppCompatActivity {
         resultTextView = findViewById(R.id.result_text_view);
     }
 
+    private double dodawanieLiczb(double first, double second){
+        return first + second;
+    }
     public void addNumbers(View view) {
-//        resultTextView.setText("Adding");
         String firstValue = firstNumberInput
                 .getText()
                 .toString();
@@ -33,10 +38,13 @@ public class MainActivity extends AppCompatActivity {
         if (!firstValue.isEmpty() && !secondValue.isEmpty()){
             firstNumber = Double.parseDouble(firstValue);
             secondNumber = Double.parseDouble(secondValue);
-            showResult(Double.toString(firstNumber + secondNumber));
+            showResult(Double.toString(dodawanieLiczb(firstNumber, secondNumber)));
         }
     }
 
+    private double odejmowanieLiczb(double first, double second){
+        return first-second;
+    }
     public void subNumbers(View view) {
 //        resultTextView.setText("Subtraction");
         String firstValue = firstNumberInput
@@ -48,8 +56,11 @@ public class MainActivity extends AppCompatActivity {
         if (!firstValue.isEmpty() && !secondValue.isEmpty()){
             firstNumber = Double.parseDouble(firstValue);
             secondNumber = Double.parseDouble(secondValue);
-            showResult(Double.toString(firstNumber - secondNumber));
+            showResult(Double.toString(odejmowanieLiczb(firstNumber, secondNumber)));
         }
+    }
+    private double mnozenieLiczb(double first, double second){
+        return first * second;
     }
     public void mulNumbers(View view) {
         String firstValue = firstNumberInput
@@ -61,11 +72,12 @@ public class MainActivity extends AppCompatActivity {
         if (!firstValue.isEmpty() && !secondValue.isEmpty()){
             firstNumber = Double.parseDouble(firstValue);
             secondNumber = Double.parseDouble(secondValue);
-            showResult(Double.toString(firstNumber * secondNumber));
+            showResult(Double.toString(mnozenieLiczb(firstNumber, secondNumber)));
         }
-//        resultTextView.setText("Multiplication");
     }
-
+    private double dzielenieLiczb(double first, double second){
+        return first / second;
+    }
     public void divNumbers(View view) {
         String firstValue = firstNumberInput
                 .getText()
@@ -76,14 +88,12 @@ public class MainActivity extends AppCompatActivity {
         if (!firstValue.isEmpty() && !secondValue.isEmpty()){
             firstNumber = Double.parseDouble(firstValue);
             secondNumber = Double.parseDouble(secondValue);
-
             if (secondNumber != 0){
-                showResult(Double.toString(firstNumber /secondNumber));
+                showResult(Double.toString(dzielenieLiczb(firstNumber, secondNumber)));
             } else {
                 showResult("Nie dziel przez zero !");
             }
         }
-//        resultTextView.setText("Division");
     }
 
     public void showResult(String resultString){
@@ -91,5 +101,57 @@ public class MainActivity extends AppCompatActivity {
         if (!resultString.isEmpty()){
             resultTextView.setText(resultString);
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Date currentTime = Calendar.getInstance().getTime();
+        Log.d("onPause", "onPause method called at " + currentTime.toString());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Date currentTime = Calendar.getInstance().getTime();
+        Log.d("onResume", "onResume method called at " + currentTime.toString());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Date currentTime = Calendar.getInstance().getTime();
+        Log.d("onDestroy", "onDestroy method called at " + currentTime.toString());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Date currentTime = Calendar.getInstance().getTime();
+        Log.d("onStop", "onStop method called at " + currentTime.toString());
+
+        // ERROR MESSAGES
+        //Log.e();
+
+        // WARNING MESSAGES
+        //Log.w();
+
+        // INFORMATION MESSAGES
+        //Log.i(TAG, "onStop: ");
+
+        // DEBUG MESSAGES
+        //Log.d();
+
+        // VERBOSE MESSAGES
+        //Log.v();
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Date currentTime = Calendar.getInstance().getTime();
+        Log.d("onStart", "onStart method called at " + currentTime.toString());
     }
 }
