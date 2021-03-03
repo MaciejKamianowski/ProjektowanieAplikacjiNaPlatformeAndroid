6d81a4785e677318d74e96db0405e7440a7d7b59
Maciej Kamianowski <2605mac@gmail.com>
2021-03-03 09:09:44 +0100

    Dodano funkcje do przycisków oraz walidację danych

--- Zadanie1/app/src/main/java/com/mk/zadanie1/MainActivity.java
+++ Zadanie1/app/src/main/java/com/mk/zadanie1/MainActivity.java
@@ -10,29 +10,86 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private EditText firstNumberInput;
     private EditText secondNumberInput;
-    //private TextView resultTextView;
+    private TextView resultTextView;
+    private double firstNumber;
+    private double secondNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         firstNumberInput = findViewById(R.id.first_number_edit_text);
         secondNumberInput = findViewById(R.id.second_number_edit_text);
-        //resultTextView = findViewById(R.id.result_text_view);
+        resultTextView = findViewById(R.id.result_text_view);
     }
 
     public void addNumbers(View view) {
 //        resultTextView.setText("Adding");
+        String firstValue = firstNumberInput
+                .getText()
+                .toString();
+        String secondValue = secondNumberInput
+                .getText()
+                .toString();
+        if (!firstValue.isEmpty() && !secondValue.isEmpty()){
+            firstNumber = Double.parseDouble(firstValue);
+            secondNumber = Double.parseDouble(secondValue);
+            showResult(Double.toString(firstNumber + secondNumber));
+        }
     }
 
     public void subNumbers(View view) {
 //        resultTextView.setText("Subtraction");
+        String firstValue = firstNumberInput
+                .getText()
+                .toString();
+        String secondValue = secondNumberInput
+                .getText()
+                .toString();
+        if (!firstValue.isEmpty() && !secondValue.isEmpty()){
+            firstNumber = Double.parseDouble(firstValue);
+            secondNumber = Double.parseDouble(secondValue);
+            showResult(Double.toString(firstNumber - secondNumber));
+        }
     }
-
     public void mulNumbers(View view) {
+        String firstValue = firstNumberInput
+                .getText()
+                .toString();
+        String secondValue = secondNumberInput
+                .getText()
+                .toString();
+        if (!firstValue.isEmpty() && !secondValue.isEmpty()){
+            firstNumber = Double.parseDouble(firstValue);
+            secondNumber = Double.parseDouble(secondValue);
+            showResult(Double.toString(firstNumber * secondNumber));
+        }
 //        resultTextView.setText("Multiplication");
     }
 
     public void divNumbers(View view) {
+        String firstValue = firstNumberInput
+                .getText()
+                .toString();
+        String secondValue = secondNumberInput
+                .getText()
+                .toString();
+        if (!firstValue.isEmpty() && !secondValue.isEmpty()){
+            firstNumber = Double.parseDouble(firstValue);
+            secondNumber = Double.parseDouble(secondValue);
+
+            if (secondNumber != 0){
+                showResult(Double.toString(firstNumber /secondNumber));
+            } else {
+                showResult("Nie dziel przez zero !");
+            }
+        }
 //        resultTextView.setText("Division");
+    }
+
+    public void showResult(String resultString){
+        //wyświetl wynik w polu result_text_view
+        if (!resultString.isEmpty()){
+            resultTextView.setText(resultString);
+        }
     }
 }
